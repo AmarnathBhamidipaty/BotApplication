30fdfd86614a412d997ea94a10a3081cca9d0cb7
AmarnathBhamidipaty <amarnth630@gmail.com>
2018-09-20 18:48:01 +0530

    Master commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=evolve404appservice-Web-Deploy.pubxml -p:Password=lAdRKi2Hpp8mv4yC1EWRfsXkYQGniubWWhx5cf83LaJCuqlzkvtmxwTG0xL5
+
